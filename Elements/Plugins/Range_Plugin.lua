9990882700351fad529c9c3995084c85d80fc61a
Raeli <42666479+Caedilla@users.noreply.github.com>
2019-09-20 09:19:35 +0200

    Make Range Checking smarter

--- Elements/Plugins/Range_Plugin.lua
+++ Elements/Plugins/Range_Plugin.lua
@@ -4,6 +4,150 @@ local libRangeCheck = LibStub("LibRangeCheck-2.0")
 local updateFrequency = 0.25 -- TODO Add Option somewhere
 local _FRAMES = {}
 local OnRangeFrame
+local _,uClass = UnitClass('player')
+
+
+local FriendSpells = {}
+local HarmSpells = {}
+
+FriendSpells["DEATHKNIGHT"] = {
+}
+HarmSpells["DEATHKNIGHT"] = {
+	49576, -- ["Death Grip"], -- 30
+}
+
+FriendSpells["DEMONHUNTER"] = {
+}
+HarmSpells["DEMONHUNTER"] = {
+	185123, -- ["Throw Glaive"], -- 30
+}
+
+FriendSpells["DRUID"] = {
+	774, -- ["Rejuvenation"], -- 40
+	2782, -- ["Remove Corruption"], -- 40
+}
+HarmSpells["DRUID"] = {
+	5176, -- ["Wrath"], -- 40
+	339, -- ["Entangling Roots"], -- 35
+	6795, -- ["Growl"], -- 30
+	33786, -- ["Cyclone"], -- 20
+	22568, -- ["Ferocious Bite"], -- 5
+}
+
+FriendSpells["HUNTER"] = {}
+HarmSpells["HUNTER"] = {
+	75, -- ["Auto Shot"], -- 40
+}
+
+FriendSpells["MAGE"] = {
+}
+HarmSpells["MAGE"] = {
+	44614, --["Frostfire Bolt"], -- 40
+	5019, -- ["Shoot"], -- 30
+}
+
+FriendSpells["MONK"] = {
+	115450, -- ["Detox"], -- 40
+	115546, -- ["Provoke"], -- 30
+}
+HarmSpells["MONK"] = {
+	115546, -- ["Provoke"], -- 30
+	115078, -- ["Paralysis"], -- 20
+	100780, -- ["Tiger Palm"], -- 5
+}
+
+FriendSpells["PALADIN"] = {
+	19750, -- ["Flash of Light"], -- 40
+}
+HarmSpells["PALADIN"] = {
+	62124, -- ["Reckoning"], -- 30
+	20271, -- ["Judgement"], -- 30
+	853, -- ["Hammer of Justice"], -- 10
+	35395, -- ["Crusader Strike"], -- 5
+}
+
+FriendSpells["PRIEST"] = {
+	527, -- ["Purify"], -- 40
+	17, -- ["Power Word: Shield"], -- 40
+}
+HarmSpells["PRIEST"] = {
+	589, -- ["Shadow Word: Pain"], -- 40
+	5019, -- ["Shoot"], -- 30
+}
+
+FriendSpells["ROGUE"] = {}
+HarmSpells["ROGUE"] = {
+	2764, -- ["Throw"], -- 30
+	2094, -- ["Blind"], -- 15
+}
+
+FriendSpells["SHAMAN"] = {
+	8004, -- ["Healing Surge"], -- 40
+	546, -- ["Water Walking"], -- 30
+}
+HarmSpells["SHAMAN"] = {
+	403, -- ["Lightning Bolt"], -- 40
+	370, -- ["Purge"], -- 30
+	73899, -- ["Primal Strike"],. -- 5
+}
+
+FriendSpells["WARRIOR"] = {}
+HarmSpells["WARRIOR"] = {
+	355, -- ["Taunt"], -- 30
+	100, -- ["Charge"], -- 8-25
+	5246, -- ["Intimidating Shout"], -- 8
+}
+
+FriendSpells["WARLOCK"] = {
+	5697, -- ["Unending Breath"], -- 30
+}
+HarmSpells["WARLOCK"] = {
+	686, -- ["Shadow Bolt"], -- 40
+	5019, -- ["Shoot"], -- 30
+}
+
+
+
+local function IsUnitInRange(unit)
+	local canAttack = UnitCanAttack('player', unit)
+	local canHelp = UnitCanAssist('player', unit)
+	local isFriend = UnitIsFriend('player', unit)
+	local isVisible = UnitIsVisible(unit)
+	local rangeSpells, minRange, maxRange
+	local connected = UnitIsConnected(unit)
+	if not connected then return true end
+	if isVisible then
+		if canAttack then
+			rangeSpells = HarmSpells[uClass]
+		elseif canHelp then
+			rangeSpells = FriendSpells[uClass]
+		end
+		if canHelp or canAttack then
+			for i = 1,#rangeSpells do
+				if IsSpellKnown(rangeSpells[i]) then
+					if IsSpellInRange(GetSpellInfo(rangeSpells[i]),unit) == 1 then
+						return true
+					end
+				end
+			end
+		end
+		if canAttack then
+			minRange, maxRange = libRangeCheck:GetRange(unit,true)
+			if maxRange < 30 then
+				return true
+			end
+		end
+		if canHelp then
+			minRange, maxRange = libRangeCheck:GetRange(unit,true)
+			if maxRange < 40 then
+				return true
+			end
+		end
+		if not canHelp and not canAttack and isFriend then return true end -- We can't reliably get range on units we cannot interact with so don't fade the frame out.
+	end
+
+	return false
+end
 
 local function Update(self, event)
 	local element = self.RangeCheck
@@ -15,29 +159,13 @@ local function Update(self, event)
 	end
 
 	if element.enabled == true then
-		local minRange, maxRange
-		local connected = UnitIsConnected(unit)
-		if(connected) then
-			local isEnemy = UnitCanAttack('player', unit)
-			local isFriend = UnitCanAssist('player', unit)
-			minRange, maxRange = libRangeCheck:GetRange(unit,true)
-			if minRange and not maxRange then
-				maxRange = minRange
-			end
-			if maxRange then
-				if (not isEnemy and maxRange > 40) or (isEnemy and maxRange > 30) or (not isEnemy and not isFriend and maxRange >= 28) then
-					self:SetAlpha(currentAlpha * element.outsideAlpha)
-				else
-					self:SetAlpha(currentAlpha * element.insideAlpha)
-				end
-			else
-				self:SetAlpha(currentAlpha * element.insideAlpha)
-			end
-		else
+		if IsUnitInRange(unit) then
 			self:SetAlpha(currentAlpha * element.insideAlpha)
+		else
+			self:SetAlpha(currentAlpha * element.outsideAlpha)
 		end
 		if(element.PostUpdate) then
-			return element:PostUpdate(self, minRange, maxRange, connected)
+			return element:PostUpdate(self, unit)
 		end
 	else
 		self:SetAlpha(currentAlpha * element.insideAlpha)
