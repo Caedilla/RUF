527f9d60e8c96f4741b4d352c7802346f6a6a28d
Raeli <42666479+Caedilla@users.noreply.github.com>
2020-10-06 14:38:05 +0200

    build seperately

--- Release/release.sh
+++ Release/release.sh
@@ -1,2 +1,8 @@
-curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh| bash -s -- -p 283389 -g 1.13.5 -o -m classic.pkgmeta
-curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh| bash -s -- -p 283389 -g 8.3.7 -o -m live.pkgmeta
+if [ "$1" == "classic" ]
+then
+  curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh| bash -s -- -p 283389 -g 1.13.5 -o -m classic.pkgmeta
+elif [ "$1" == "live" ]
+  curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh| bash -s -- -p 283389 -g 8.3.7 -o -m live.pkgmeta
+else
+ echo "Game Type not specified."
+fi
