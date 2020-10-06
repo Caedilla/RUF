a57c9f58f320325abcbf757110cd6d9921d5f6a5
Raeli <42666479+Caedilla@users.noreply.github.com>
2020-10-06 16:11:44 +0200

    hopefully this cleans up the release now

--- Release/release.sh
+++ Release/release.sh
@@ -1,11 +1,2 @@
-if [ "$1" == "classic" ]
-then
-  echo "Package and Release for Classic"
-  curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh| bash -s -- -p 283389 -g 1.13.5 -o -m classic.pkgmeta
-elif [ "$1" == "live" ]
-then
-  echo "Package and Release for Retail"
-  curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh| bash -s -- -p 283389 -g 8.3.7 -o -m live.pkgmeta
-else
-  echo "Game Type not specified."
-fi
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh| bash -s -- -p 283389 -g 1.13.5 -o -m classic.pkgmeta
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh| bash -s -- -p 283389 -g 8.3.0 -o -m live.pkgmeta
